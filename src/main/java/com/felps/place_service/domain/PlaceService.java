290a220c3d4d42e71f42d139d10523769513e27c
fabio <fabiofelipevianasantosfacul@gmail.com>
2024-11-05 00:02:25 -0300

    Place Service creation

---
+++ src/main/java/com/felps/place_service/domain/PlaceService.java
@@ -0,0 +1,16 @@
+package com.felps.place_service.domain;
+
+import reactor.core.publisher.Mono;
+
+public class PlaceService {
+    private PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    public Mono<Place> create(Place place){
+        return placeRepository.save(place);
+    }
+
+}
