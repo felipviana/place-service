f77bd3380bf2651fbb830932d140f405954b16e9
fabio <fabiofelipevianasantosfacul@gmail.com>
2024-11-05 00:40:22 -0300

    Rest of the CRUDE creation and final adjustments

--- src/main/java/com/felps/place_service/domain/PlaceService.java
+++ src/main/java/com/felps/place_service/domain/PlaceService.java
@@ -2,22 +2,43 @@ package com.felps.place_service.domain;
 
 import com.felps.place_service.api.PlaceRequest;
 import com.github.slugify.Slugify;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Comparator;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
     private Slugify slg;
 
-
-    public PlaceService(PlaceRepository placeRepository) {
-        this.placeRepository = placeRepository;
+    public PlaceService(PlaceRepository placeRepository){
+        this.placeRepository=placeRepository;
         this.slg = Slugify.builder().build();
+    }
+    public Mono<Place> findById(Long id){
+        return placeRepository.findById(id);
+    }
+
+    public Flux<Place> list(){
+        return placeRepository.findAll()
+                .sort(Comparator.comparing(Place::name));
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null,placeRequest.name(), slg.slugify(placeRequest.name()),
-                placeRequest.state(),null,null);
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(), null,null);
         return placeRepository.save(place);
+    }
+
+    public Mono<Place> update(PlaceRequest placeRequest){
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(), null, null);
+        return placeRepository.save(place);
+    }
+
+    public Mono<Void> delete(Long id){
+        return placeRepository.deleteById(id)
+                .onErrorResume(e -> Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Place not found")));
     }
 
 }
--- src/main/java/com/felps/place_service/web/PlaceController.java
+++ src/main/java/com/felps/place_service/web/PlaceController.java
@@ -2,31 +2,55 @@ package com.felps.place_service.web;
 
 import com.felps.place_service.api.PlaceRequest;
 import com.felps.place_service.api.PlaceResponse;
-import com.felps.place_service.domain.Place;
 import com.felps.place_service.domain.PlaceService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/places")
 public class PlaceController {
 
+
+    @Autowired
     private PlaceService placeService;
 
-    public PlaceController(PlaceService placeService) {
-        this.placeService = placeService;
+
+    @GetMapping
+    public ResponseEntity<Flux<PlaceResponse>> findAll(){
+        Flux<PlaceResponse> places = placeService.list().map(PlaceMapper::fromPlaceToResponse);
+        return ResponseEntity.ok(places);
     }
 
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<PlaceResponse>> findById(@PathVariable Long id){
+        return placeService.findById(id)
+                .map(PlaceMapper::fromPlaceToResponse)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+
+    }
     @PostMapping
     public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request){
         var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
     }
 
+    @PutMapping
+    public ResponseEntity<Mono<PlaceResponse>> update(@Valid @RequestBody PlaceRequest request){
+        var placeResponse = placeService.update(request).map(PlaceMapper::fromPlaceToResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(placeResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Object>> delete(@PathVariable Long id){
+        return placeService.delete(id)
+                .then(Mono.just(ResponseEntity.noContent().build()))
+                .onErrorResume(ResponseStatusException.class, e -> Mono.just(ResponseEntity.notFound().build()));
+    }
 }
