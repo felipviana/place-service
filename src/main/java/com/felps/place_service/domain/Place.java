3927914bf58d510dcbbceb14e71f548242032951
fabio <fabiofelipevianasantosfacul@gmail.com>
2024-11-05 00:19:36 -0300

    Auditing configuration

--- src/main/java/com/felps/place_service/domain/Place.java
+++ src/main/java/com/felps/place_service/domain/Place.java
@@ -1,10 +1,19 @@
 package com.felps.place_service.domain;
 
 import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record Place(
-        Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        Long id,
+        String name,
+        String slug,
+        String state,
+        @CreatedDate
+        LocalDateTime createdAt,
+        @LastModifiedDate
+        LocalDateTime updatedAt) {
 }
